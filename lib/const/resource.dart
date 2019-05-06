969c4a6fb99cb24437d388067a85f51dceece0e8
Caijinglong <cjl_spy@163.com>
2019-05-06 16:05:07 +0800

    init commit

---
+++ lib/const/resource.dart
@@ -0,0 +1,9 @@
+/// generate by resouce_generator library, shouldn't edit.
+class R {
+    
+  /// ![preview](file:///private/tmp/flare_example/./asset/Smiley%20Switch.flr)
+  static const String ASSET_SMILEY_SWITCH_FLR = "asset/Smiley Switch.flr";
+    
+  /// ![preview](file:///private/tmp/flare_example/./asset/counter-binary.flr)
+  static const String ASSET_COUNTER_BINARY_FLR = "asset/counter-binary.flr";
+}
